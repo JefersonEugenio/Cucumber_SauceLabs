9bf4c3249065244e88f38eecb0150300398732ab
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-01-20 10:44:26 -0300

    Remove 'WebDriverManager.chromedriver().setuo()' e adicionei 'ChromeOptions'

--- src/main/java/framework/supports/Hooks.java
+++ src/main/java/framework/supports/Hooks.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
@@ -16,8 +17,9 @@ public class Hooks {
 
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        WebDriver driver = new ChromeDriver(options);
         scenarioContext.setDriver(driver);
         driver.get("https://www.saucedemo.com/v1/index.html");
     }
