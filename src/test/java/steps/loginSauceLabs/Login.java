5af003bd9229ff51f0e2e9c22e565c51377127b1
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2025-01-21 11:52:57 -0300

    Tirei comentario e log

--- src/test/java/steps/loginSauceLabs/Login.java
+++ src/test/java/steps/loginSauceLabs/Login.java
@@ -2,8 +2,10 @@ package steps.loginSauceLabs;
 
 import framework.supports.BaseTest;
 import framework.supports.ScenarioContext;
+import framework.supports.ScenarioManager;
 import framework.webDrivers.DriverManager;
 import framework.webDrivers.Drivers;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
@@ -21,7 +23,6 @@ public class Login {
 
     @Dado("que o usuario esta na pagina de login do sistema Sauce Labs")
     public void que_o_usuario_esta_na_pagina_de_login_do_sistema_sauce_labs() {
-        System.out.println("A tela está pagina de login do sistema Sauce Labs");
     }
 
     @Quando("o usuario preenche o campo {string} com {string}")
@@ -29,26 +30,23 @@ public class Login {
         WebDriver driver = scenarioContext.getDriver();
         if (campo.equalsIgnoreCase("username")) {
             driver.findElement(By.id("user-name")).sendKeys(string);
-            System.out.println("Preenche do usuario: " + string);
         } else if (campo.equalsIgnoreCase("password")) {
             driver.findElement(By.id("password")).sendKeys(string);
-            System.out.println("Preenche a senha do usuario: " + string);
         }
     }
-//
-//    @E("o usuario clica no botao login")
-//    public void o_usuario_clica_no_botao_login() {
-//        driver.findElement(By.id("login-button")).click();
-//    }
-//
-//    @Entao("o sistema autentica o usuario")
-//    public void o_sistema_autentica_o_usuario() {
-//        System.out.println("Entrando autentica");
-//    }
-//
-//    @E("redireciona para a tela principal")
-//    public void redireciona_para_a_tela_principal() {
-//        System.out.println("A tela está principal a página do sistema Sauce Labs");
-//    }
+
+    @E("o usuario clica no botao login")
+    public void o_usuario_clica_no_botao_login() {
+        WebDriver driver = scenarioContext.getDriver();
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Entao("o sistema autentica o usuario")
+    public void o_sistema_autentica_o_usuario() {
+    }
+
+    @E("redireciona para a tela principal")
+    public void redireciona_para_a_tela_principal() {
+    }
 
 }
