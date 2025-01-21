6c3d5aeb8564ee684f1c26de52f6caf51c76ea9e
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-01-21 12:10:27 -0300

    Adicionei dois BDD

--- src/test/java/steps/loginSauceLabs/Login.java
+++ src/test/java/steps/loginSauceLabs/Login.java
@@ -5,10 +5,7 @@ import framework.supports.ScenarioContext;
 import framework.webDrivers.DriverManager;
 import framework.webDrivers.Drivers;
 import io.cucumber.java.Scenario;
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.E;
-import io.cucumber.java.pt.Entao;
-import io.cucumber.java.pt.Quando;
+import io.cucumber.java.pt.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -34,6 +31,16 @@ public class Login {
         }
     }
 
+    @E("o usuário deixa o campo {string} em branco")
+    public void o_usuário_deixa_o_campo_password_em_branco(String campo) {
+        WebDriver driver = scenarioContext.getDriver();
+        if (campo.equalsIgnoreCase("username")) {
+            driver.findElement(By.id("user-name")).clear();
+        } else if (campo.equalsIgnoreCase("password")) {
+            driver.findElement(By.id("password")).clear();
+        }
+    }
+
     @E("o usuario clica no botao login")
     public void o_usuario_clica_no_botao_login() {
         WebDriver driver = scenarioContext.getDriver();
@@ -48,4 +55,9 @@ public class Login {
     public void redireciona_para_a_tela_principal() {
     }
 
+    @Então("o sistema exibe a mensagem {string}")
+    public void o_sistema_exibe_a_mensagem(String mensagem) throws IllegalAccessException {
+        WebDriver driver = scenarioContext.getDriver();
+        String imprimir = driver.findElement(By.cssSelector("#login_button_container > div > form > h3 > button")).getText();
+    }
 }
